f142c9fb73a340b57388b4abab1c386b25cf3fd7
indre101 <zygaityte.indre@gmail.com>
2020-03-03 15:26:40 +0100

    adds border radius

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-window.addEventListener("DOMContentLoaded", init)
+window.addEventListener("DOMContentLoaded", init);
 
-function getHTMLelements(params) {
-
-}
-
+function getHTMLelements(params) {}
 
 async function init() {
   const getSVG = await fetch("./cat.svg");
   const response = await getSVG.text();
   document.querySelector(".imageTemplate").innerHTML = response;
-  assignDifferentColors()
+  assignDifferentColors();
+  getColorChoice();
+  colorTheArea();
 }
 
-function assignDifferentColors() {
-  document.querySelectorAll(".colorSwatch").forEach(swatch => swatch.style.fill = `rgb(${getRandomRGBColor().r}, ${getRandomRGBColor().g}, ${getRandomRGBColor().b})`);
-}
+const assignDifferentColors = () =>
+  document
+  .querySelectorAll(".colorSwatch")
+  .forEach(
+    swatch =>
+    (swatch.style.fill = `rgb(${getRandomRGBColor().r}, ${
+          getRandomRGBColor().g
+        }, ${getRandomRGBColor().b})`)
+  );
 
-function getRandomRGBColor() {
+const getRandomRGBColor = () => {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
   const b = Math.floor(Math.random() * 255);
@@ -25,5 +30,32 @@ function getRandomRGBColor() {
     r,
     g,
     b
-  }
+  };
+};
+
+const getColorChoice = () => {
+  document
+    .querySelectorAll(".colorSwatch")
+    .forEach(colorChoice => colorChoice.addEventListener("click", pickUpColor));
+};
+
+let colorPick;
+
+const pickUpColor = () => {
+  colorPick = event.target.style.fill;
+  document.querySelector(".pickedColor").style.backgroundColor = colorPick;
+  return colorPick;
+};
+
+function colorTheArea() {
+  document
+    .querySelectorAll("[data-name=coloring-space]")
+    .forEach(colorArea => colorArea.addEventListener("click", executeColorIng));
 }
+
+const executeColorIng = () => {
+  console.log("object");
+  console.log(colorPick);
+
+  event.target.style.fill = colorPick;
+};
